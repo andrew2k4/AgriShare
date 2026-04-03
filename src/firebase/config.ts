0384825f76b230a4682816cb997b9ccdcd110476
Wilfrid Fosso Takam (AZIM) <fossowilfrid@gmail.com>
2026-04-03 18:10:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2061268298-75f07",
+  "appId": "1:1046988288095:web:07a5efb552e85cabf02bea",
+  "apiKey": "AIzaSyCLPy-FcrH9JWanO1P_BKTI-qw-G9_azIA",
+  "authDomain": "studio-2061268298-75f07.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1046988288095"
+};
